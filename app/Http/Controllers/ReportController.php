3d8e644a25f544d4c77a23e37f45922d8cc50746
30mda <dev.ahmedemad@gmail.com>
2022-10-09 17:15:14 +0200

    Display all relation types even they doesn't exist at report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -17,23 +17,23 @@ class ReportController extends Controller
     public function index(Request $request)
     {
         $sponsor_types = DB::table('sponsor_types')
-            ->join('reports','sponsor_types.id', '=', 'reports.sponsor_type_id')
-            ->select('sponsor_types.name', DB::raw('count(*) as total'))
+            ->leftJoin('reports','sponsor_types.id', '=', 'reports.sponsor_type_id')
+            ->select('sponsor_types.name', DB::raw('count(reports.sponsor_type_id) as total'))
             ->groupBy('sponsor_types.name')
             ->get();
         $locations = DB::table('locations')
-            ->join('reports','locations.id', '=', 'reports.location_id')
-            ->select('locations.name', DB::raw('count(*) as total'))
+            ->leftJoin('reports','locations.id', '=', 'reports.location_id')
+            ->select('locations.name', DB::raw('count(reports.location_id) as total'))
             ->groupBy('locations.name')
             ->get();
         $program_breaks = DB::table('program_breaks')
-            ->join('reports','program_breaks.id', '=', 'reports.program_break_id')
-            ->select('program_breaks.name', DB::raw('count(*) as total'))
+            ->leftJoin('reports','program_breaks.id', '=', 'reports.program_break_id')
+            ->select('program_breaks.name', DB::raw('count(reports.program_break_id) as total'))
             ->groupBy('program_breaks.name')
             ->get();
         $reruns = DB::table('reruns')
-            ->join('reports','reruns.id', '=', 'reports.rerun_id')
-            ->select('reruns.name', DB::raw('count(*) as total'))
+            ->leftJoin('reports','reruns.id', '=', 'reports.rerun_id')
+            ->select('reruns.name', DB::raw('count(reports.rerun_id) as total'))
             ->groupBy('reruns.name')
             ->get();
         $air_time = DB::table('reports')
